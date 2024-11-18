03de80960fd57cb797eb69205aff4c2a8926f526
Apfirebolt <aspper20@gmail.com>
2024-11-19 05:04:05 +0530

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+    <p class="text-red-300 bg-slate-300">
+        The ABout Page
+    </p>
+  </template>
+  <script setup>
+  
+  
+  onMounted(() => {
+    console.log('Mounted ..')
+  });
+  </script>
+  
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <p class="text-red-300 bg-slate-300">
+        The Home Page
+    </p>
+  </template>
+  <script setup>
+  
+  
+  onMounted(() => {
+    console.log('Mounted ..')
+  });
+  </script>
+  
